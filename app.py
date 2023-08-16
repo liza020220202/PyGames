6916a6c6d21c2b443e2793b3cc08a493580d0347
Liza <elizaveta.markushkina@yandex.ru>
2023-08-16 13:26:09 +0300

    moving up

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((1200, 800))
+screen = pygame.display.set_mode((700, 600))
 
 rect = pygame.Rect(50, 100, 50, 100)
-pygame.draw.rect(screen, (0, 255, 0), rect)
+
 
 while True:
-    pygame.display.flip()
+    screen.fill('black')
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYUP:
+            rect.move_ip(0, -20)
+
+    pygame.draw.rect(screen, (0, 255, 0), rect)
+    pygame.display.flip()
