8bb0f37f6640a65dc2acbb2e7d5f95f52fcca63a
Liza <elizaveta.markushkina@yandex.ru>
2023-08-16 13:16:00 +0300

    rectangle

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
 
+rect = pygame.Rect(50, 100, 50, 100)
+pygame.draw.rect(screen, (0, 255, 0), rect)
+
 while True:
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
