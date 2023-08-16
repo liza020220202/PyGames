3c2593efe5e092ea1f7696f2cf870295320c81fe
Liza <elizaveta.markushkina@yandex.ru>
2023-08-16 13:11:57 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import sys
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1200, 800))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
