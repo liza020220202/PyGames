1d95823a4e2416a305f817e0a40d20826c2f8a9f
Liza <elizaveta.markushkina@yandex.ru>
2023-08-16 13:32:04 +0300

    moving rect

--- app.py
+++ app.py
@@ -17,8 +17,21 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if event.type == pygame.KEYUP:
-            rect.move_ip(0, -20)
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_UP:
+                rect.move_ip(0, -20)
+
+            if event.key == pygame.K_DOWN:
+                rect.move_ip(0, 20)
+
+            if event.key == pygame.K_LEFT:
+                rect.move_ip(-20, 0)
+
+            if event.key == pygame.K_RIGHT:
+                rect.move_ip(20, 0)
+
+
 
     pygame.draw.rect(screen, (0, 255, 0), rect)
     pygame.display.flip()
